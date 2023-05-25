3e4d82835edbc98a51e68319bc5e669521b42ed1
ExDrill <72329798+ExDrill@users.noreply.github.com>
2023-05-25 16:43:06 -0500

    Project Setup!

--- src/main/java/com/teamabode/guarding/Guarding.java
+++ src/main/java/com/teamabode/guarding/Guarding.java
@@ -2,7 +2,7 @@ package com.teamabode.template;
 
 import net.fabricmc.api.ModInitializer;
 
-public class TemplateMod implements ModInitializer {
+public class Guarding implements ModInitializer {
 
     public void onInitialize() {
 
